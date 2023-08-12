9b972e8ffc81e8d2246e10019ae04ab669f43345
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-08-12 17:54:30 +0530

    Modified the Sentiment API

--- controller/sentimentController.js
+++ controller/sentimentController.js
@@ -93,6 +93,7 @@ const transformersApiUrl = 'https://api-inference.huggingface.co/models/distilbe
 const analyzeSentiments = async (answers) => {
   try {
     const sentiments = [];
+    //const scores = [];
 
     for (const answer of answers) {
       const response = await fetch(transformersApiUrl, {
@@ -114,10 +115,21 @@ const analyzeSentiments = async (answers) => {
       const data = result[0];
 
       if (Array.isArray(result)) {
+        let sentiment = '';
+        let score = 0;
         const scoreLabel0 = data[0].score;
         const scoreLabel1 = data[1].score;
-        const sentiment = scoreLabel0 >= scoreLabel1 ? data[0].label : data[1].label;
-        sentiments.push(sentiment);
+        if (scoreLabel0 >= scoreLabel1) {
+          sentiment = data[0].label;
+          score = scoreLabel0;
+        }
+        else {
+          sentiment = data[1].label;
+          score = scoreLabel1;
+        }
+        //const sentiment = scoreLabel0 >= scoreLabel1 ? data[0].label : data[1].label;
+        sentiments.push(sentiment, score);
+        
       } else {
         throw new Error('Invalid response format from Hugging Face API');
       }
