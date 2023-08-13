3229b48ed2ec44197d42fb3c7d4efd71ae592fd4
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-08-13 14:26:37 +0530

    Models APIs are edited 3

--- controller/modelsController.js
+++ controller/modelsController.js
@@ -1,70 +1,28 @@
-// modelController.js
-const path = require('path');
-const fs = require('fs');
-const faceapi = require('@vladmandic/face-api');
-const { Canvas, Image, ImageData } = require('canvas');
-faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
-
-// Load the models
-async function loadModels() {
-    const modelPaths = {
-        tinyFaceDetector: 'models/tiny_face_detector_model-weights_manifest.json',
-        faceLandmark68Net: 'models/face_landmark_68_model-weights_manifest.json',
-        faceRecognitionNet: 'models/face_recognition_model-weights_manifest.json',
-        faceExpressionNet: 'models/face_expression_model-weights_manifest.json',
-    };
-
-    await Promise.all(
-        Object.values(modelPaths).map(async (modelPath) => {
-            const filePath = path.join(__dirname, '..', modelPath);
-            if (fs.existsSync(filePath)) {
-                await faceapi.nets.tinyFaceDetector.loadFromDisk(filePath);
-                await faceapi.nets.faceLandmark68Net.loadFromDisk(filePath);
-                await faceapi.nets.faceRecognitionNet.loadFromDisk(filePath);
-                await faceapi.nets.faceExpressionNet.loadFromDisk(filePath);
-            } else {
-                console.error('Model file not found:', filePath);
-                throw new Error(`Model file not found: ${filePath}`);
-            }
-        })
-    );
+const axios = require('axios');
+const emotionAnalysisApiUrl='https://api.deepai.org/api/facial-expression-recognition';
+async function analyzeEmotionForFrame(emotionAnalysisApiUrl, frame) {
+  try {
+    const formData = new FormData();
+    formData.append('frame', frame, 'emotion-frame.jpg');
+    const response = await axios.post(emotionAnalysisApiUrl, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+    return response.data.emotions;
+  } catch (error) {
+    console.error('Error analyzing emotion for frame:', error);
+    return { message: 'Failed to analyze emotion for frame' };
+  }
 }
 
-// Analyze emotions in the provided image
-async function computeEmotion(req, res) {
-    try {
-        // Ensure that the models are loaded
-        await loadModels();
-
-        // Read the image data from the request
-        const imageBase64 = req.body.image;
-        const imageData = Buffer.from(imageBase64, 'base64');
-
-        // Load the image as a tensor
-        const image = await faceapi.bufferToImage(imageData);
-
-        // Detect faces and analyze emotions
-        const detections = await faceapi.detectAllFaces(image).withFaceExpressions();
-
-        if (detections.length === 0) {
-            return res.json({ message: 'No face detected in the image' });
-        }
-
-        // Get the emotion analysis results for the first detected face
-        const expression = detections[0].expressions;
-        const emotions = Object.keys(expression).reduce((result, expr) => {
-            result[expr] = expression[expr];
-            return result;
-        }, {});
-
-        // Send the emotion analysis results to the frontend
-        res.json({ emotions });
-    } catch (error) {
-        console.error('Error computing emotion:', error);
-        res.status(500).json({ message: 'Failed to compute emotion' });
-    }
+async function analyzeEmotionForVideoFrames(emotionAnalysisApiUrl, frames) {
+  const emotions = [];
+  for (const frame of frames) {
+    const frameEmotions = await analyzeEmotionForFrame(emotionAnalysisApiUrl, frame);
+    emotions.push(frameEmotions);
+  }
+  return emotions;
 }
 
 module.exports = {
-    computeEmotion
+  analyzeEmotionForVideoFrames,
 };
--- routes/modelRoutes.js
+++ routes/modelRoutes.js
@@ -1,9 +1,20 @@
-// routes.js
 const express = require('express');
-const router = express.Router();
+const multer = require('multer');
 const modelController = require('../controller/modelsController');
 
-// POST endpoint for computing emotion
-router.post('/computeEmotion', modelController.computeEmotion);
+const router = express.Router();
+const upload = multer();
+
+router.post('/analyzeVideoEmotions', upload.array('frames'), async (req, res) => {
+  try {
+    const emotionAnalysisApiUrl = 'http://example.com/emotion-analysis-api'; // Replace with the actual API URL
+    const frames = req.files.map((file) => file.buffer);
+    const emotions = await modelController.analyzeEmotionForVideoFrames(emotionAnalysisApiUrl, frames);
+    res.json({ emotions });
+  } catch (error) {
+    console.error('Error analyzing video emotions:', error);
+    res.status(500).json({ message: 'Failed to analyze video emotions' });
+  }
+});
 
 module.exports = router;
