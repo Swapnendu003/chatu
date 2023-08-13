799ab5e4de3a412c202d4be93c7690876a5c9153
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-08-13 13:46:00 +0530

    Models APIs are edited 3

--- controller/modelsController.js
+++ controller/modelsController.js
@@ -1,41 +1,70 @@
 // modelController.js
 const path = require('path');
 const fs = require('fs');
+const faceapi = require('@vladmandic/face-api');
+const { Canvas, Image, ImageData } = require('canvas');
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
-function loadModels(req, res) {
-    try {
-        // Define the paths to the model files
-        const modelPaths = {
-            tinyFaceDetector: 'models/tiny_face_detector_model-weights_manifest.json',
-            faceLandmark68Net: 'models/face_landmark_68_model-weights_manifest.json',
-            faceRecognitionNet: 'models/face_recognition_model-weights_manifest.json',
-            faceExpressionNet: 'models/face_expression_model-weights_manifest.json',
-        };
+// Load the models
+async function loadModels() {
+    const modelPaths = {
+        tinyFaceDetector: 'models/tiny_face_detector_model-weights_manifest.json',
+        faceLandmark68Net: 'models/face_landmark_68_model-weights_manifest.json',
+        faceRecognitionNet: 'models/face_recognition_model-weights_manifest.json',
+        faceExpressionNet: 'models/face_expression_model-weights_manifest.json',
+    };
 
-        // Read the model files
-        const models = {};
-        for (const [modelName, modelPath] of Object.entries(modelPaths)) {
+    await Promise.all(
+        Object.values(modelPaths).map(async (modelPath) => {
             const filePath = path.join(__dirname, '..', modelPath);
-            console.log('Checking file:', filePath);
-            const folderPath = path.join(__dirname, '..', 'models');
-    const filesInFolder = fs.readdirSync(folderPath);
-    console.log('Files in "models" folder:', filesInFolder);
             if (fs.existsSync(filePath)) {
-                models[modelName] = fs.readFileSync(filePath, 'utf8');
+                await faceapi.nets.tinyFaceDetector.loadFromDisk(filePath);
+                await faceapi.nets.faceLandmark68Net.loadFromDisk(filePath);
+                await faceapi.nets.faceRecognitionNet.loadFromDisk(filePath);
+                await faceapi.nets.faceExpressionNet.loadFromDisk(filePath);
             } else {
                 console.error('Model file not found:', filePath);
-                return res.status(500).json({ message: `Model file not found: ${modelName}` });
+                throw new Error(`Model file not found: ${filePath}`);
             }
+        })
+    );
+}
+
+// Analyze emotions in the provided image
+async function computeEmotion(req, res) {
+    try {
+        // Ensure that the models are loaded
+        await loadModels();
+
+        // Read the image data from the request
+        const imageBase64 = req.body.image;
+        const imageData = Buffer.from(imageBase64, 'base64');
+
+        // Load the image as a tensor
+        const image = await faceapi.bufferToImage(imageData);
+
+        // Detect faces and analyze emotions
+        const detections = await faceapi.detectAllFaces(image).withFaceExpressions();
+
+        if (detections.length === 0) {
+            return res.json({ message: 'No face detected in the image' });
         }
 
-        // Send the models to the frontend
-        res.json(models);
+        // Get the emotion analysis results for the first detected face
+        const expression = detections[0].expressions;
+        const emotions = Object.keys(expression).reduce((result, expr) => {
+            result[expr] = expression[expr];
+            return result;
+        }, {});
+
+        // Send the emotion analysis results to the frontend
+        res.json({ emotions });
     } catch (error) {
-        console.error('Error loading models:', error);
-        res.status(500).json({ message: 'Failed to load models' });
+        console.error('Error computing emotion:', error);
+        res.status(500).json({ message: 'Failed to compute emotion' });
     }
 }
 
 module.exports = {
-    loadModels
+    computeEmotion
 };
--- routes/modelRoutes.js
+++ routes/modelRoutes.js
@@ -1,9 +1,9 @@
-// modelRoutes.js
+// routes.js
 const express = require('express');
 const router = express.Router();
 const modelController = require('../controller/modelsController');
 
-// Route to load the face detection models
-router.get('/loadModels', modelController.loadModels);
+// POST endpoint for computing emotion
+router.post('/computeEmotion', modelController.computeEmotion);
 
 module.exports = router;
